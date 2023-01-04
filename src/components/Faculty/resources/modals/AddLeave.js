65454394d4edcba1a07d783d2992a7f7ed0a52c5
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-01-04 21:01:35 +0500

    Add leave modal form created.

--- src/components/Faculty/resources/modals/AddLeave.js
+++ src/components/Faculty/resources/modals/AddLeave.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import Modal from 'react-modal'
+import TextField from '@material-ui/core/TextField'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 function AddLeave(props) {
 
@@ -24,8 +30,37 @@ function AddLeave(props) {
                 style={customStyles}
                 isOpen={openLeaveModal}
                 onRequestClose={() => setLeaveModal(false)}>
-                <div className='center'>
+                <div className='center flexbox-container-y'>
                     <h2 style={{ color: "#115868", fontSize: 20 }}>Add Leave</h2>
+                    <form>
+                        <TextField
+                            style={{ marginBottom: '1rem' }}
+                            label="Reason"
+                            defaultValue="Add text here..."
+                            variant="outlined"
+                            multiline={true}
+                            rows={4} />
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <Stack spacing={1.5}>
+                                <DesktopDatePicker
+                                    label="Date"
+                                    inputFormat="DD/MM/YYYY"
+                                    renderInput={(params) => <TextField {...params}
+                                        variant="outlined" />} />
+                                <TimePicker
+                                    label="Start Time"
+                                    renderInput={(params) => <TextField {...params}
+                                        variant="outlined" />} />
+                                <TimePicker
+                                    label="End Time"
+                                    renderInput={(params) => <TextField {...params}
+                                        variant="outlined" />} />
+                            </Stack>
+                        </LocalizationProvider>
+                        <div className='center flexbox-container-y'>
+                            <button style={{ marginTop: '1rem' }} type='submit' className='modal-btn'>Save</button>
+                        </div>
+                    </form>
                 </div>
             </Modal>
         </div>
