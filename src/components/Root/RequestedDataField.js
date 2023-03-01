f478c85cf640c486bd493fb3d2bff9878b4c3c73
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-03-01 15:45:07 +0500

    Delete icon added on requested data fields.

--- src/components/Root/RequestedDataField.js
+++ src/components/Root/RequestedDataField.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styling/HomeScreen.css'
 import { useSelector } from 'react-redux'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function RequestedDataField(props) {
 
@@ -9,20 +10,23 @@ function RequestedDataField(props) {
     const isAdmin = useSelector((state) => state.login.user._admin)
 
     return (
-        <div key={index} className="col-data" style={{marginTop: '10px'}}>
-            <div className='align'>
-                <div className="circle">
-                    R
+        <div key={index} className="col-data container" style={{marginTop: '10px'}}>
+                <div className='align left-div'>
+                    <div className="circle">
+                        R
+                    </div>
+                    <div style={{marginLeft: '15px'}}>
+                        <h5>{name}</h5>
+                        <h6>{details}</h6>
+                        {
+                            isAdmin ? <h6>{from}</h6> : null
+                        }
+                        <h6>{date} | {startTime} - {endTime}</h6>
+                    </div>
                 </div>
-                <div style={{marginLeft: '15px'}}>
-                    <h5>{name}</h5>
-                    <h6>{details}</h6>
-                    {
-                        isAdmin ? <h6>{from}</h6> : null
-                    }
-                    <h6>{date} | {startTime} - {endTime}</h6>
+                <div className='align-y right-div'>
+                    <DeleteIcon className='delete-btn' />
                 </div>
-            </div>
         </div>
     )
 }
