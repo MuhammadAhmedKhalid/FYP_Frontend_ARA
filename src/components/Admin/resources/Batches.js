4d5db322bfb28196be5383afe6b7d875284a7ab4
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-03-08 17:23:33 +0500

    Showing added batches in the table format.

--- src/components/Admin/resources/Batches.js
+++ src/components/Admin/resources/Batches.js
@@ -1,17 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminIcon from '../AdminIcon'
 import AdminNavBar from '../AdminNavbar'
 import AddBatches from './modals/AddBatches'
 import Table from '../../Root/Table'
+import { useSelector, useDispatch } from 'react-redux'
+import { getBatchesRequest } from '../../../redux/GetBatches/getBatchesActions'
 
 function Batches() {
+
+    const dispatch = useDispatch()
+
+    const batches = useSelector((state) => state.getBatchesReducer.batches.data)
+    const batchesAdded = useSelector((state) => state.getBatchesReducer.added)
+    const institute_name = useSelector((state) => state.login.user.institute_name)
+    const institute_id = useSelector((state) => state.login.user.institute_id)
+
     const [openBatchModal, setOpenBatchModal] = useState(false)
+    const [rowData, setRowData] = useState([])
+    const [refresh, setRefresh] = useState(false)
+
+    useEffect(()=>{
+        if(batchesAdded && rowData.length !== batches.length){
+            for(let i=0; i<batches.length; i++){
+                rowData.push([batches[i].batchYear, institute_name])
+            }
+        }
+    },[batchesAdded, batches, institute_name, rowData])
+
+    useEffect(() => {
+        if(institute_id > 0){
+            dispatch(getBatchesRequest(institute_id))
+        }
+    }, [refresh, institute_id, dispatch])
 
     const openModal = () => {
         setOpenBatchModal(true)
     }
-
-    const [rowData, setRowData] = useState([])
 
     return (
         <div>
@@ -33,7 +57,7 @@ function Batches() {
                 </div>
             </div>
             <div>
-                <AddBatches openBatchModal={openBatchModal} setOpenBatchModal={setOpenBatchModal} />
+                <AddBatches openBatchModal={openBatchModal} setOpenBatchModal={setOpenBatchModal} setRefresh={setRefresh}/>
             </div>
         </div>
     )
