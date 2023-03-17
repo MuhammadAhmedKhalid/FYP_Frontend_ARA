a3407f1ed778456ba4e1f988d198175c42ae36e6
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-03-17 17:29:39 +0500

    Showing all events on schedule.

--- src/components/Faculty/Schedule.js
+++ src/components/Faculty/Schedule.js
@@ -1,13 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import FacultyNavbar from './FacultyNavbar'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import { format } from 'date-fns';
+import { useSelector, useDispatch } from 'react-redux'
+import { assignedCoursesRequest } from '../../redux/AssignedCourses/assignedCoursesActions'
+import { getCourseRequest } from '../../redux/GetCourse/getCourseActions'
 
 function Schedule() {
 
     const localizer = momentLocalizer(moment)
+    const dispatch = useDispatch()
 
+    const [events, setEvents] = useState([])
+
+    const institute_id = useSelector((state) => state.login.user.institute_id)
+    const assignedCourses = useSelector((state) => state.assignedCoursesReducer.assignedCourses.data)
+    const assignedCoursesAdded = useSelector((state) => state.assignedCoursesReducer.added)
+    const courses = useSelector((state) => state.getCourseReducer.courses)
+    const coursessAdded = useSelector((state) => state.getCourseReducer.added)
+
+    useEffect(() => {
+        if(institute_id){
+            dispatch(assignedCoursesRequest(institute_id))
+            dispatch(getCourseRequest(institute_id))
+        }
+    }, [])
+
+    useEffect(() => {
+        if(assignedCoursesAdded && coursessAdded && assignedCourses.length > 0 && courses.length > 0){
+            setEvents([])
+            for(let i of assignedCourses){
+                for(let j of courses){
+                    if(j.course_id == i.course_id){
+                        setEvents(events => [...events, {title: j.course_name, startDate: new Date(i.date + " " +i.startTime), endDate: new Date(i.date + " " +i.endTime)}])
+                    }
+                    break
+                }
+            }
+        }
+    }, [assignedCourses, courses])
+    
     return (
         <div className="flexbox-container-y"
             style={{
@@ -21,18 +54,9 @@ function Schedule() {
                 style={{ height: 600, margin: "100px" }}
                 messages={{ today: 'Current' }}
                 localizer={localizer}
-                // events={events}
-                // views={['month']}
+                events={events}
                 startAccessor="startDate"
                 endAccessor="endDate"
-                selectable
-                // onSelectEvent={
-
-                // event => showDetails(event)
-                // }
-                // onSelectSlot={
-                // slotInfo => change(slotInfo)
-                // }
             />
         </div>
     )
