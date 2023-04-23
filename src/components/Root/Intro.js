b1d4b55ebad461144201928a71826ccb03a270d8
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-04-23 22:39:04 +0500

    Responsiveness added for Intro component.

--- src/components/Root/Intro.js
+++ src/components/Root/Intro.js
@@ -1,37 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Img1 from '../../assets/img1.png'
-
-const heading = {
-    fontSize: '60px'
-}
+import '../Styling/IntroStyling.css'
 
 function Intro() {
 
-    const [click, setClick] = useState(false)
-    const [color, setColor] = useState(false)
-
-    const handleClick = () => setClick(!click)
-    const changeColor = () => {
-        if (window.scrollY >= 100) {
-            setColor(true);
-        } else {
-            setColor(false);
-        }
-    }
+    const isMobile = window.innerWidth <= 1040;
 
     return (
         <div
-            className="flexbox-container"
+            // className="flexbox-container"
             style={{
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
                 height: '100vh'
-            }}>
+            }}
+            >
             <div>
-                <h1 style={heading}>Auto Resource</h1>
-                <h1 style={heading}>Allocator</h1>
+                <h1 className='heading'>Auto Resource</h1>
+                <h1 className='heading'>Allocator</h1>
                 <br /><br />
+                {isMobile ? <img className="img" src={Img1} alt='Into-img' /> : null}
+                <br/>
                 <p>With Auto Resource Allocator, you can focus on what matters most</p>
                 <p>delivering quality education, products, or services</p>
                 <p>while we take care of the rest. Sign up today to see</p>
@@ -42,7 +32,7 @@ function Intro() {
                 <br />
             </div>
             <div>
-                <img style={{ width: 500 }} src={Img1} alt='Into-img' />
+                {isMobile ? null : <img className="image" src={Img1} alt='Into-img' />}
             </div>
         </div>
     )
