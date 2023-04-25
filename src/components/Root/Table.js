7ba2c8f0217c685a8c9e5d9f824355c8f6d76b5b
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-04-25 16:39:09 +0500

    Refreshing styling added.

--- src/components/Root/Table.js
+++ src/components/Root/Table.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styling/TableStyles.css'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import { height } from '@mui/system';
 
 function Table(props) {
 
     const { columns, rows } = props
+
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleDelete = () => {
         console.log('Delete')
@@ -18,40 +19,49 @@ function Table(props) {
     }
 
     const handleRefresh = () => {
-        console.log('Refresh')
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+          }, 1000);
     }
 
     return (
-        <div className="table-container" style={{ marginTop: '30px' }}>
-            <div className='refreshContainer'><RefreshIcon onClick={handleRefresh} className='refreshButton'/></div>
-            <table className="table">
-                <thead className="fixed-header">
-                    <tr>
+        <div>
+            {isLoading && (
+                <div className="loading-overlay">
+                <div className="loading-icon"></div>
+                </div>
+            )}
+            <div className="table-container" style={{ marginTop: '30px' }}>
+                <div className='refreshContainer'><RefreshIcon onClick={handleRefresh} className='refreshButton'/></div>
+                <table className="table">
+                    <thead className="fixed-header">
+                        <tr>
+                            {
+                                columns.map((column, index) => (<th key={index}>{column}</th>))
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
                         {
-                            columns.map((column, index) => (<th key={index}>{column}</th>))
+                            rows.map((cellData, index) => (
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    {
+                                        cellData.map((data, dataIndex)=>(
+                                            <td key={dataIndex}>
+                                                {data}
+                                                <DeleteIcon onClick={handleDelete} className='deleteButton'/>
+                                                <EditIcon onClick={handleEdit} className='editButton'/>
+                                            </td>
+                                        ))
+                                    }
+                                </tr>
+                            ))
                         }
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        rows.map((cellData, index) => (
-                            <tr key={index}>
-                                <td>{index+1}</td>
-                                {
-                                    cellData.map((data, dataIndex)=>(
-                                        <td key={dataIndex}>
-                                            {data}
-                                            <DeleteIcon onClick={handleDelete} className='deleteButton'/>
-                                            <EditIcon onClick={handleEdit} className='editButton'/>
-                                        </td>
-                                    ))
-                                    
-                                }
-                            </tr>
-                          ))
-                    }
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
