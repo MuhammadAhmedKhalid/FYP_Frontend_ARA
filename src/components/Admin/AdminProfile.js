9c6e691e62e6600d2537896900a1f2f88a7bd700
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-05-04 06:04:31 +0500

    Showing details on admin profile.

--- src/components/Admin/AdminProfile.js
+++ src/components/Admin/AdminProfile.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminNavBar from './AdminNavbar'
 import '../Styling/Profile.css'
+import { getInstitutesRequest } from '../../redux/GetInstitutes/getInstitutesActions'
+import { getInstituteTypeRequest } from '../../redux/InstituteTypes/instituteTypesActions'
+import { useSelector, useDispatch } from 'react-redux'
 
 function AdminProfile() {
 
-  const [name, setName] = useState("");
-  const [institute_name, setInstituteName] = useState("")
-  const [email, setEmail] = useState("");
+  const dispatch = useDispatch()
+
+  const institutes = useSelector((state) => state.getInstitutes.institutes.data)
+  const isInstitutesAdded = useSelector((state) => state.getInstitutes.added)
+  const types = useSelector((state) => state.instituteType.instituteTypes)
+
+  const [name, setName] = useState(localStorage.getItem('name'));
+  const [institute_name, setInstituteName] = useState(localStorage.getItem('institute_name'))
+  const [email, setEmail] = useState(localStorage.getItem('email'));
   const [instituteType, setInstituteType] = useState("")
   const [branch, setBranch] = useState("");
   const [address, setAddress] = useState("");
   const [contact, setContact] = useState("");
   const [isEditMode, setIsEditMode] = useState(false);
   const [isInstEditMode, setIsInstEditMode] = useState(false);
+  const institute_id = localStorage.getItem('institute_id')
+
+  useEffect(() => {
+    dispatch(getInstitutesRequest())
+    dispatch(getInstituteTypeRequest())
+  }, [])
+
+  useEffect(() => {
+    if(isInstitutesAdded){
+      for(let i of institutes){
+        for(let j of types){
+          if(institute_id == i.institute_id && i.institute_type_id === j.institute_type_id){
+            setAddress(i.address)
+            setBranch(i.branch)
+            setContact(i.contact)
+            setInstituteType(j.name)
+          }
+        }
+      }
+    }
+  }, [institutes])
 
   const handleEditClick = () => {
     setIsEditMode(!isEditMode);
