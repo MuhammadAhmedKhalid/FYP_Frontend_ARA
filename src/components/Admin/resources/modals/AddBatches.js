7392b8b81a4f701a6d560d2aac6a4c6ebae02a74
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-11 04:29:22 +0500

    New fields added in add batches modal.

--- src/components/Admin/resources/modals/AddBatches.js
+++ src/components/Admin/resources/modals/AddBatches.js
@@ -6,6 +6,11 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import { useSelector, useDispatch } from 'react-redux'
 import { addBatchtRequest, resetState } from '../../../../redux/AddBatch/addBatchActions'
 import { Alert } from '@mui/material';
+import CodeIcon from '@mui/icons-material/Code';
+import TagIcon from '@mui/icons-material/Tag';
+import WorkspacesIcon from '@mui/icons-material/Workspaces';
+import { departmentsList } from '../../utils'
+import { specializationOptions } from '../../utils'
 
 function AddBatches(props) {
 
@@ -15,16 +20,21 @@ function AddBatches(props) {
 
     const [showError, setShowError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('')
+    const [specialization, setSpecialization] = useState([])
 
-    const departments = useSelector((state) => state.getDepartments.departments.data)
-    const departmentsAdded = useSelector((state) => state.getDepartments.added)
+    // const departments = useSelector((state) => state.getDepartments.departments.data)
+    // const departmentsAdded = useSelector((state) => state.getDepartments.added)
+    const departments = departmentsList
     const institute_id = Number(localStorage.getItem('institute_id'))
     const requestSuccessfull = useSelector((state) => state.addBatchReducer.added)
     const requestUnsuccessfullMsg = useSelector((state) => state.addBatchReducer.error)
 
     const [batch, setBatch] = useState({
         batchYear: "",
-        department_id: "",
+        batchCode: "",
+        noOfStudents: "",
+        section: "",
+        department: "",
         institute_id,
     })
 
@@ -56,16 +66,22 @@ function AddBatches(props) {
     };
     const handleForm = (event) => {
         event.preventDefault()
-        dispatch(addBatchtRequest(batch))
+        console.log(batch)
+        // dispatch(addBatchtRequest(batch))
     }
 
     const handleDepartmentChange = (e) => {
         const department_name = e.target.value
-            for (let i = 0; i < departments.length; i++) {
-                if (departments[i].department_name === department_name) {
-                    setBatch({ ...batch, department_id: departments[i].department_id })
+            for (let i = 0; i < specializationOptions.length; i++) {
+                if (specializationOptions[i].department === department_name) {
+                    // setBatch({ ...batch, department_id: specializationOptions[i].department_id })
+                    setSpecialization(specializationOptions[i].specialization)
                 }
             }
+    }
+
+    const handleSpecializationChange = (e) => {
+        setBatch({ ...batch, department: e.target.value })
     }
 
     const closeModal = () => {
@@ -84,12 +100,39 @@ function AddBatches(props) {
                 <div className='center flexbox-container-y'>
                     <h2 style={{ color: "#115868", fontSize: 20 }}>Add Batch</h2>
                     <form onSubmit={handleForm}>
-                        <TextField required autoFocus style={{ margin: '3px' }} size='small' variant="outlined" type='number' placeholder='Batch Year.' 
+                        <TextField required autoFocus style={{ margin: '3px' }} size='small' variant="outlined" type='number' placeholder='Batch year' 
                         onChange={(e) => setBatch({ ...batch, batchYear: e.target.value })}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position='start'>
                                     <GroupsIcon style={{ height: '20px' }} color="action" />
+                                </InputAdornment>
+                            )
+                        }} />
+                        <TextField required style={{ margin: '3px' }} size='small' variant="outlined" type='text' placeholder='Batch code' 
+                        onChange={(e) => setBatch({ ...batch, batchCode: e.target.value })}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <CodeIcon style={{ height: '20px' }} color="action" />
+                                </InputAdornment>
+                            )
+                        }} />
+                        <TextField required style={{ margin: '3px' }} size='small' variant="outlined" type='number' placeholder='Number of students' 
+                        onChange={(e) => setBatch({ ...batch, noOfStudents: e.target.value })}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <TagIcon style={{ height: '20px' }} color="action" />
+                                </InputAdornment>
+                            )
+                        }} />
+                        <TextField required style={{ margin: '3px' }} size='small' variant="outlined" type='text' placeholder='Section' 
+                        onChange={(e) => setBatch({ ...batch, section: e.target.value })}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <WorkspacesIcon style={{ height: '20px' }} color="action" />
                                 </InputAdornment>
                             )
                         }} />
@@ -100,8 +143,20 @@ function AddBatches(props) {
                             <select required className='dropdown' onChange={handleDepartmentChange}>
                             <option></option>
                                 {
-                                    departmentsAdded && departments.length !== 0 ? departments.map(department => 
-                                        <option key={department.department_id}>{department.department_name}</option>) : null
+                                    departments.length !== undefined ? departments.map(department => 
+                                        <option key={department.id}>{department.name}</option>) : null
+                                }
+                            </select>
+                        </div>
+                        <div style={{ margin: '3px' }} className='flexbox-container-y'>
+                            <h3 style={{
+                                fontWeight: 'normal', color: 'gray', marginRight: '3px'
+                            }}>Specialization</h3>
+                            <select required className='dropdown' onChange={handleSpecializationChange}>
+                            <option></option>
+                                {
+                                    specialization.length !== 0 ? specialization.map((specialization, index) => 
+                                        <option key={index}>{specialization}</option>) : null
                                 }
                             </select>
                         </div>
