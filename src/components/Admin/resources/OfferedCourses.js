970b2f54c585f22434ebe7f262faac4e8d1ea8b6
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-25 01:07:00 +0500

    Offer course modal form fields added.

--- src/components/Admin/resources/OfferedCourses.js
+++ src/components/Admin/resources/OfferedCourses.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminIcon from '../AdminIcon'
 import AdminNavBar from '../AdminNavbar'
 import Table from '../../Root/Table'
 import OfferCourse from './modals/OfferCourse'
+import { useSelector, useDispatch } from 'react-redux'
+import { getCourseRequest } from '../../../redux/GetCourse/getCourseActions'
+import { getBatchesRequest } from '../../../redux/GetBatches/getBatchesActions'
+import { getDepartmentsRequest } from '../../../redux/GetDepartments/getDepartmentsActions'
 
 function OfferedCourses() {
+
+    const dispatch = useDispatch()
+
+    const courses = useSelector((state) => state.getCourseReducer.courses)
+    const coursessAdded = useSelector((state) => state.getCourseReducer.added)
+    const batches = useSelector((state) => state.getBatchesReducer.batches.data)
+    const batchesAdded = useSelector((state) => state.getBatchesReducer.added)
+    const departments = useSelector((state) => state.getDepartments.departments.data)
+    const departmentsAdded = useSelector((state) => state.getDepartments.added)
+
+    const institute_id = localStorage.getItem('institute_id')
 
     const [openOfferCourseModal, setOpenOfferCourseModal] = useState(false)
     const [refresh, setRefresh] = useState(false)
@@ -12,6 +27,14 @@ function OfferedCourses() {
     const [update, setUpdate] = useState(false)
     const [deleteId, setDeleteId] = useState(null)
     const [data, setData] = useState()
+
+    useEffect(() => {
+        if(institute_id > 0){
+            dispatch(getCourseRequest(institute_id))
+            dispatch(getBatchesRequest(institute_id))
+            dispatch(getDepartmentsRequest(institute_id))
+        }
+    }, [refresh, institute_id])
 
     const openModal = () => {
         setOpenOfferCourseModal(true)
