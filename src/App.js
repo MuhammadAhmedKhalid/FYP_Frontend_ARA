86033fa74c46c684f934232f189ad6b52f41fc79
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-31 07:33:21 +0500

    hash router added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import LandingPage from './components/Root/LandingPage';
 import Modal from 'react-modal'
 import NoMatch from './components/Root/NoMatch';
@@ -34,10 +34,10 @@ function App() {
   return (
     <div>
       <Provider store={store}>
-        <BrowserRouter basename='/FYP_Frontend_ARA'>
+        <HashRouter basename='/FYP_Frontend_ARA'>
         <Routes>
 
-        <Route path='/FYP_Frontend_ARA' element={<LandingPage />} />
+        <Route exact path='/FYP_Frontend_ARA' element={<LandingPage />} />
 
         {isAdmin === 'false' ? <Route path='*' element={<NoMatch />}/> : <Route path='/admin' element={<AuthRoute><AdminHomeScreen /></AuthRoute>} />}
         {isAdmin === 'false' ? <Route path='*' element={<NoMatch />}/> : <Route path='/domains' element={<AuthRoute><Domains /></AuthRoute>} /> }
@@ -62,7 +62,7 @@ function App() {
         <Route path='*' element={<NoMatch />} />
 
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </div>
   );
