ca8e697117ceaef80b50f4df94659624dcd8609c
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-31 14:06:28 +0500

    App js and package update.

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       <Provider store={store}>
         <Routes>
 
-        <Route exact path='/FYP_Frontend_ARA' element={<LandingPage />} />
+        <Route exact path='/' element={<LandingPage />} />
 
         {isAdmin === 'false' ? <Route path='*' element={<NoMatch />}/> : <Route path='/admin' element={<AuthRoute><AdminHomeScreen /></AuthRoute>} />}
         {isAdmin === 'false' ? <Route path='*' element={<NoMatch />}/> : <Route path='/domains' element={<AuthRoute><Domains /></AuthRoute>} /> }
